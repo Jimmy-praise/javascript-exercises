1f9579141da4dd5b09a17d9396d37ba847933e35
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-12-05 13:36:08 +0100

    Done with 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = ''
+    if (num < 0) {
+        text = 'ERROR'
+    } else {
+        for (let i = 0; i < num; i++) {
+        text += string
+    }
+    }
+    return text
 };
+
+repeatString('hey', 3)
+repeatString('hey', 10)
+repeatString('hey', 1)
+repeatString('hey', 0)
+repeatString('hey', -1)
+
+let randomNumber = Math.random() * 10
+repeatString('hey', randomNumber)
+
 
 // Do not edit below this line
 module.exports = repeatString;
