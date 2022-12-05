01dc670c52f57dfa30fb817cd5ea1724e0c7eb46
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-12-05 14:05:02 +0100

    Done with 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let reversedStringArray = stringArray.reverse()
+    return reversedString = reversedStringArray.join('')
 };
 
 // Do not edit below this line
