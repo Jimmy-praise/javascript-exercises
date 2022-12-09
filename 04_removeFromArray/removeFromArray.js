0cd06779e5ba590a399f285b7e62b1ec46ba8afc
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-12-09 14:21:31 +0100

    Done with 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...inputsToBeRemoved) {
+    for (let inputToBeRemoved of inputsToBeRemoved) {
+        let indexOfInputToBeRemoved = array.indexOf(inputToBeRemoved);
+        if (indexOfInputToBeRemoved == -1) {
+            continue
+        } else {
+            array.splice(indexOfInputToBeRemoved, 1);
+        };
+    }
+    return array;
 };
 
 // Do not edit below this line
