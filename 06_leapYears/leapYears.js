c68a55533e3cff9b7520ac343ecf5b408f068ac1
PJimmy_McP_Small <Jimmlius923@gmail.com>
2022-12-12 14:55:10 +0100

    Done with 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let yearDividedByFour = year / 4;
+    let yearDividedByOneHundred = year / 100;
+    let yearDividedByFourHundred = year / 400;
+    if (Math.floor(yearDividedByFourHundred) == yearDividedByFourHundred) {
+            return true;
+        } 
+        else if (Math.floor(yearDividedByOneHundred) == yearDividedByOneHundred) {
+            return false;
+        } 
+        else if (Math.floor(yearDividedByOneHundred) == yearDividedByOneHundred && 
+        Math.floor(yearDividedByFourHundred) != yearDividedByFourHundred) {
+            return false;
+        }
+        else if (Math.floor(yearDividedByFour) == yearDividedByFour) {
+            return true;
+        }
+        else if (Math.floor(yearDividedByFourHundred) != yearDividedByFourHundred || 
+        Math.floor(yearDividedByOneHundred) != yearDividedByOneHundred || 
+        Math.floor(yearDividedByFour) == yearDividedByFour) {
+            return false;
+        }
 };
 
 // Do not edit below this line
