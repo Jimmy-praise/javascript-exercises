d806ce33d1b2a5470c58f309b9aea57feb657564
PJimmy_McP_Small <Jimmlius923@gmail.com>
2023-01-23 14:06:11 +0100

    Done with 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringArray = string.split('');
+    let lettersArray = stringArray.filter(
+        letter => {
+            if (letter.includes(' ') || letter.includes(',') || letter.includes('.') || letter.includes('!')) {
+                return false
+            } else {
+                return true;
+            }
+        }
+    )
+    let stringLetters = (lettersArray.join('')).toLowerCase();
 
+    let backwardLettersArray = lettersArray.reverse();
+    let stringBackwardLetters = (backwardLettersArray.join('')).toLowerCase();
+
+    return stringLetters == stringBackwardLetters ? true : false;
 };
 
 // Do not edit below this line
