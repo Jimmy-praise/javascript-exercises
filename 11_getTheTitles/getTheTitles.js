bcd30187d473c204e9e3495b727e6db84c26b91b
PJimmy_McP_Small <Jimmlius923@gmail.com>
2023-01-26 15:10:42 +0100

    Done with 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titlesArray = [];
+    let titles = array.map(obj => titlesArray.push(obj['title'])
+    );
+    return titlesArray;
 };
 
 // Do not edit below this line
