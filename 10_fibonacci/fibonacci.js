11999b75a39fdbc43d33e36b252d755bea75c0ad
PJimmy_McP_Small <Jimmlius923@gmail.com>
2023-01-26 15:07:55 +0100

    Done with 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+number = parseInt(number);
+let series = [0, 1, 1];
+if (number < 0) {
+    return "OOPS"
+};
+for (let i = 1; i <= number; i++) {
+    let newNumber = series[series.length - 1] + series[series.length - 2];
 
+    series.push(newNumber);
+};
+return series[number];
 };
 
 // Do not edit below this line
