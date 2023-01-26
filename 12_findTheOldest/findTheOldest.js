fc4478b4ec37d6469357161eff050d0387210118
PJimmy_McP_Small <Jimmlius923@gmail.com>
2023-01-26 15:13:21 +0100

    Done with 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sortedAge = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = (new Date()).getFullYear();
+        };
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        };
+        a = a.yearOfDeath - a.yearOfBirth;
+        b = b.yearOfDeath - b.yearOfBirth;
+        return a > b ? -1 : 1;
+    })
+    return sortedAge[0];
 };
 
 // Do not edit below this line
